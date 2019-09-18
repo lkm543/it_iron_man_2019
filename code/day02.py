129fea6e8106234610ea11a9108e362723910c4f
lkm543 <lkm543@gmail.com>
2019-09-18 08:24:58 +0800

    Create day02.py

---
+++ code/day02.py
@@ -0,0 +1,33 @@
+import time
+
+
+class Transaction:
+    def __init__(self, sender, receiver, amounts, fee, message):
+        self.sender = sender
+        self.receiver = receiver
+        self.amounts = amounts
+        self.fee = fee
+        self.message = message
+
+
+class Block:
+    def __init__(self, previous_hash, difficulty, miner, miner_rewards):
+        self.previous_hash = previous_hash
+        self.hash = ''
+        self.difficulty = difficulty
+        self.nonce = 0
+        self.timestamp = int(time.time())
+        self.transactions = []
+        self.miner = miner
+        self.miner_rewards = miner_rewards
+
+
+class BlockChain:
+    def __init__(self):
+        self.adjust_difficulty_blocks = 10
+        self.difficulty = 1
+        self.block_time = 30
+        self.miner_rewards = 10
+        self.block_limitation = 32
+        self.chain = []
+        self.pending_transactions = []
